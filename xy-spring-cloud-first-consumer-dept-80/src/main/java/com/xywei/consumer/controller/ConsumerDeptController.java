8b45ce0784a3eb235c7c7d2ceedb09356b4d2854
winfuture <insoul_net@qq.com>
2019-08-25 14:26:02 +0800

    完成provider暴露服务给client

--- xy-spring-cloud-first-consumer-dept-80/src/main/java/com/xywei/consumer/controller/ConsumerDeptController.java
+++ xy-spring-cloud-first-consumer-dept-80/src/main/java/com/xywei/consumer/controller/ConsumerDeptController.java
@@ -1,8 +1,10 @@
 package com.xywei.consumer.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,6 +47,16 @@ public class ConsumerDeptController {
 		return restTemplate.getForObject(PROVIDER_URL_SUFFIX + "dept" + "/" + id, Dept.class);
 //		 这里有个坑，provider端明明是get要求，此处也是get请求，最后怎么出现了405，原因，是方法用错了
 //		return restTemplate.getForObject(PROVIDER_URL_SUFFIX+"dept", Dept.class, id);
+	}
+
+	@GetMapping("/consumer/service")
+	public List<List<ServiceInstance>> serviceDiscovery() {
+
+		List<List<ServiceInstance>> allInstancesList = restTemplate.getForObject(PROVIDER_URL_SUFFIX + "/service",
+				List.class);
+
+		return allInstancesList;
+
 	}
 
 }
